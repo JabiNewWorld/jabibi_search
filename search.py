0a14eaefa27e7311f725b10af869c3e602f943fc
JabibiCODE <101432325+JabibiCODEPR@users.noreply.github.com>
2022-03-19 14:01:11 -0400

    Create search.py

---
+++ search.py
@@ -0,0 +1,71 @@
+VERSION = "1.0.0"
+CREATOR = "AKA $JABIBI"
+
+
+from os import system
+from pdb import main
+from re import search
+from winreg import QueryInfoKey
+import discord
+import json
+import requests
+import time
+from discord import webhook as req
+from pywhatkit import search
+from colorama import Fore, Back, Style
+import discord, ctypes, json, os, webbrowser, requests, datetime, urllib, time, string, random, asyncio, aiohttp
+
+
+os.system("cls")
+
+   
+os.system("mode con: cols=80 lines=25")
+ctypes.windll.kernel32.SetConsoleTitleW(f"JABIBI SEARCH PROGRAMM | VERSION 1.0.0")
+
+
+print("""""
+      +-+-+-+-+-+-+ +-+-+-+-+-+-+
+>>>>> |J|A|B|I|B|I| |S|E|A|R|C|H| <<<<<<<<
+      +-+-+-+-+-+-+ +-+-+-+-+-+-+
+      
+""""")
+time.sleep(1)
+print("SIGUEME EN MIS REVsSqDrN2snr28pSsfDiS4aYpay7dbC5a INSTAGRAM https://www.instagram.com/jabibipr/")
+time.sleep(1)
+print("MI GITHUB https://github.com/JabibiCODEPR")
+time.sleep(5)
+print("MI DISCORD ! Daddy Jabibi#9498")
+os.system("cls")
+
+time.sleep(5)
+
+print(f"""
+::::::'##::::'###::::'########::'####:'########::'####:        
+:::::: ##:::'## ##::: ##.... ##:. ##:: ##.... ##:. ##::        
+:::::: ##::'##:. ##:: ##:::: ##:: ##:: ##:::: ##:: ##::        
+:::::: ##:'##:::. ##: ########::: ##:: ########::: ##::        
+'##::: ##: #########: ##.... ##:: ##:: ##.... ##:: ##::        
+ ##::: ##: ##.... ##: ##:::: ##:: ##:: ##:::: ##:: ##::        
+. ######:: ##:::: ##: ########::'####: ########::'####:
+{Fore.RED}        {Fore.RED}    {Fore.RED}   {Fore.RED}
+:......:::..:::::..::........:::....::........:::....::        
+:'######::'########::::'###::::'########:::'######::'##::::'##:
+'##... ##: ##.....::::'## ##::: ##.... ##:'##... ##: ##:::: ##:
+ ##:::..:: ##::::::::'##:. ##:: ##:::: ##: ##:::..:: ##:::: ##:
+. ######:: ######:::'##:::. ##: ########:: ##::::::: #########:
+:..... ##: ##...:::: #########: ##.. ##::: ##::::::: ##.... ##:
+'##::: ##: ##::::::: ##.... ##: ##::. ##:: ##::: ##: ##:::: ##:
+. ######:: ########: ##:::: ##: ##:::. ##:. ######:: ##:::: ##:
+:......:::........::..:::::..::..:::::..:::......:::..:::::..::
+{Fore.BLACK}{Style.BRIGHT}{Fore.RESET}                                                                     
+   """+Fore.RESET)
+
+
+jabibi = input("QUE DESEAS BUSCAR EN GOOGLE? ")
+print("OK ESTOY BUSCANDO..... ")
+time.sleep(3)
+print("SIGO BUSCANDO")
+time.sleep(5)
+print("YA LO ENCONTRE..")
+time.sleep(2)
+search(jabibi)
